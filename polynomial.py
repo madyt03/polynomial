9c64575194ff1af6f2b5f39308db892e1def6d83
Madeleine Acevedo <madyt03@gmail.com>
2024-01-28 18:08:26 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -19,6 +19,14 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2 
+    
+    def __repr__(self):
+        return "( " + repr(self.p1) + " - " + repr(self.p2) + " )"
 
 class Mul:
     def __init__(self, p1, p2):
@@ -26,14 +34,25 @@ class Mul:
         self.p2 = p2
     
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
+        if isinstance(self.p1, (Add, Sub)):
+            if isinstance(self.p2, (Add, Sub)):
                  return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
+        if isinstance(self.p2, (Add, Sub)):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1 
+        self.p2 = p2 
+    
+    def __repr__(self):
+        return "(( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " ))"
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+
+# Adding Subtraction and Division
+poly_with_sub_div = Sub(poly, Div(Int(6), X()))
+print("Modified Polynomial:", poly_with_sub_div)
